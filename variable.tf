536983d651a446ee113cf61fe076a46a9d4963eb
“jatinbhalla” <jatinbhalla18@gmail.comgit config --global user.email jatinbhalla18@gmail.com>
2023-09-14 21:31:46 +0530

    test

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name_prefix" {
-  default       = "jatintesting_group"
+  default       = "jatintes_group"
   description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
