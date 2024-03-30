472e7b38a4002bb527a25f713c65fe9f6ebb0654
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2024-03-30 21:01:01 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>2.0"
     }
   }
 }
@@ -12,4 +12,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id   = "9112ff5f-1a9e-411d-b35f-4ec8bd46878a"
+  tenant_id         = "3f03adac-fb8a-4ad6-b7a7-f73cc0f6fd0d"
+  client_id         = "764fccda-d114-4bc9-9633-27a5258007d2"
+  client_secret     = "xXP8Q~Wd.hqn2WKcinIy6U66.DR9eK~TFl5dmbDo"
 }
