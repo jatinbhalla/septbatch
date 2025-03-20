e9519b2680dc426ab2ac0a93f6e8a9ce5b2bd02b
jatinbhalla <55462128+jatinbhalla@users.noreply.github.com>
2025-03-20 21:50:09 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,6 +14,6 @@ provider "azurerm" {
   features {}
   subscription_id   = "9112ff5f-1a9e-411d-b35f-4ec8bd46878a"
   tenant_id         = "3f03adac-fb8a-4ad6-b7a7-f73cc0f6fd0d"
-  client_id         = "764fccda-d114-4bc9-9633-27a5258007d2"
-  client_secret     = "pY28Q~W3hcXg1kD6k2~zuGA8oQlolqQOVsfWydsC"
+  client_id         = "02bfaf0a-7344-4ba5-b484-42d3e68d738e"
+  client_secret     = "~2f8Q~5NeUUplxvb8Wtr5qHskczDDNSmF_8a1a9K"
 }
